7e83652c6d0e0ea60400baf40fe3e19f7e69207c
Robin CR <robin005cr@gmail.com>
2026-09-14 23:31:59 +0530

    Adding details

--- publisher_subscriber/src/simple_subscriber.cpp
+++ publisher_subscriber/src/simple_subscriber.cpp
@@ -1,3 +1,16 @@
+/*
+ * project   : https://github.com/Robin005cr/ros2cpp
+ * file name : simple_subscriber.cpp
+ * author    : Robin CR
+ * mail id   : robinchovallurraju@gmail.com
+ * LinkedIn  : https://www.linkedin.com/in/robin-cr/
+ * portfolio : https://robin005cr.github.io/
+ *
+ * Note : If any mistakes, errors, or inconsistencies are found in the code, please feel free to mail me.
+ * Suggestions for improvements or better methods are always welcome and appreciated.
+ * I value constructive feedback and aim to continuously improve the quality of the work.
+ *
+ */
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
